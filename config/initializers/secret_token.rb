e30c2ea420e1147b161253b58b47f6ce86949eb3
RICHARD STALLMANS RIDER <deprecatedapi@inbox.com>
2013-04-10 17:02:28 -0700

    default rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DreamTrends::Application.config.secret_token = 'ceb164ddda6b9cc7b74a327fb200eca365785e76c7b29c16047c457da0653ea47ac3e3da290b1650c41edd1900331edbbaa01b6dccbfce7b7bc63265871f975e'
