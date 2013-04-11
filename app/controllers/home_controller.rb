41ec2cc634d869fac0117b858e78ab75e10ef08d
RICHARD STALLMANS RIDER <deprecatedapi@inbox.com>
2013-04-11 00:22:48 -0700

    more heroku tweaks

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def index
-    yesterday = Time.now.beginning_of_day - 1.day
+    yesterday = Time.now.beginning_of_day - 2.day
     @top_trends = Dream.trends_from_date(yesterday)
   end
 end
